5d8335cd5672b3fc1f0984f61eae4ba16ce81609
Michael Egorov <michwill@gmail.com>
2020-01-14 22:51:19 +0100

    Staking button transacts

--- common.js
+++ common.js
@@ -14,12 +14,36 @@ function init_contracts() {
     token = new web3.eth.Contract(token_abi, token_address);
 }
 
+async function approve(amount) {
+    var default_account = (await web3.eth.getAccounts())[0];
+    return new Promise(resolve => {
+        token.methods.approve(staking_escrow_address, amount)
+        .send({'from': default_account})
+        .once('transactionHash', function(hash) {resolve(true);});
+    });
+}
+
+async function handle_stake() {
+    var default_account = (await web3.eth.getAccounts())[0];
+    var balance = $('#stake-value').val();
+    var duration = parseInt($('#stake-duration').val());
+    balance = (BigInt(parseFloat(balance) * 1000) *
+               BigInt('1000000000000000000') / BigInt('1000')).toString();  // how to do this shit better??
+
+    if (await token.methods.allowance(default_account, staking_escrow_address).call() != 0)
+        await approve(0);
+    await approve(balance);
+    await staking_escrow.methods.deposit(balance, duration).send({'from': default_account});
+}
+
 async function init_ui() {
     var default_account = (await web3.eth.getAccounts())[0];
     $('#staking-account').text(default_account);
 
     var balance = (parseInt(await token.methods.balanceOf(default_account).call()) / 1e18).toFixed(3);
     $('#stake-value').val(balance);
+
+    $("#do-stake").click(handle_stake);
 }
 
 window.addEventListener('load', async () => {
